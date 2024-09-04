a6307289c855f83c2f8ec1f06695971406946afb
Cristian Di Gregorio <124940927+cris-tec@users.noreply.github.com>
2024-09-03 22:30:47 -0300

    Add files via upload

--- mainScript.js
+++ mainScript.js
@@ -1,8 +1,37 @@
-// script.js
+
 const boton = document.querySelector("#btnReceta");
+const listaDeCompras = document.getElementById('lista');
 const receta = [];
 
 boton.addEventListener("click", agregarReceta);
+
+
+
+function cargarIngredientes() {
+    const items = JSON.parse(localStorage.getItem('lista')) || [];
+    items.forEach(item => agregarIngLocalStorage(item));
+}
+
+function agregarIngLocalStorage(listaDeIngredientes) {
+    const li = document.createElement('li');
+    li.textContent = listaDeIngredientes;
+
+    const eliminar = document.createElement('button');
+    eliminar.textContent = 'Eliminar';
+    eliminar.className = 'eliminar';
+    eliminar.addEventListener('click', () => {
+        listaDeCompras.removeChild(li);
+        guardarLocalStorage(); 
+    });
+
+    li.appendChild(eliminar);
+    listaDeCompras.appendChild(li);
+}
+debugger
+function guardarLocalStorage() {
+    const items = Array.from(listaDeCompras.children).map(li => li.firstChild.textContent);
+    localStorage.setItem('lista', JSON.stringify(items));
+}
 
 
 function agregarReceta(event) {
@@ -15,17 +44,19 @@ function agregarReceta(event) {
         return;
     }
 
-    // Separar ingredientes por coma y eliminar espacios extra
+
     const listaDeIngredientes = ingrediente.split(',').map(ingrediente => ingrediente.trim().toLowerCase());
 
-    // Agregar la receta al array de recetas
+
     receta.push({ nombre: nombreReceta, ingrediente: listaDeIngredientes });
 
-    // Limpiar los campos de entrada
+
     document.getElementById('nomReceta').value = '';
     document.getElementById('ingredientes').value = '';
 
-    // Actualizar la lista de compras
+
+    agregarIngLocalStorage(listaDeIngredientes);
+    guardarLocalStorage(); 
     actualizarListado();
 }
 
@@ -33,10 +64,10 @@ function actualizarListado() {
     const listaDeCompras = document.getElementById('lista');
     listaDeCompras.innerHTML = '';
 
-    // Crear un objeto para contar las ocurrencias de cada ingrediente
+
     const conteoIngredientes = {};
 
-    // Contar ingredientes
+
     receta.forEach(rec => {
         rec.ingrediente.forEach(ingrediente => {
             if (conteoIngredientes[ingrediente]) {
@@ -47,11 +78,12 @@ function actualizarListado() {
         });
     });
 
-    // Mostrar los ingredientes y sus cantidades
-
     for (const [ingrediente, cont] of Object.entries(conteoIngredientes)) {
         const li = document.createElement('li');
         li.textContent = `${ingrediente.charAt(0).toUpperCase() + ingrediente.slice(1)} (${cont})`;
         listaDeCompras.append(li);
     }
 }
+
+
+cargarIngredientes();
